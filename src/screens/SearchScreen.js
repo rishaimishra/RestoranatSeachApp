d2c018a03fe90057cb0c66760d2197ba54e7a8fa
vikrant singh <rishimishra7872@gmai.com>
2020-07-19 23:53:46 +0530

    1st commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen=()=>{
+    return <View>
+        <Text>THis is Seach bar Screen</Text>
+    </View>
+}
+
+export default SearchScreen;
